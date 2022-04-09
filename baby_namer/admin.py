20efe2786bcb32515b6efea72012ba5fa0f258fc
Dyshine Hill <eyedolarts@gmail.com>
2022-04-09 13:13:35 -0500

    created serializer and name model + added app to settings + creat database

--- baby_namer/admin.py
+++ baby_namer/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Baby_namer
 # Register your models here.
+
+
--- baby_namer/models.py
+++ baby_namer/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Baby_namer:
+    name = models.CharField(_(""), max_length=50)
+    
