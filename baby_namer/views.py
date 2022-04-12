b5b69512f74081db80a4bc0b0f6bf8bf31cc699a
Dyshine Hill <eyedolarts@gmail.com>
2022-04-11 23:50:09 -0500

    fixed bug with get request for random name

--- baby_namer/views.py
+++ baby_namer/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 import random
+from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from .serializer import NameSerializer
@@ -8,5 +8,10 @@ from .models import Baby_namer
 
 # Create your views here.
 @api_view(['GET'])
-def random_name():
-    pass
+def random_name(request):
+    names = Baby_namer.objects.all()
+    name_list=list(names)
+    ran_name=random.choice(name_list)
+    serializer = NameSerializer(ran_name)
+    return Response(serializer.data)
+    
