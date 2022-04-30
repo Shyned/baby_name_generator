7e4b7b868d05ee45a58af4d83cf3f5cd0f25a915
Dyshine Hill <eyedolarts@gmail.com>
2022-04-30 16:49:26 -0500

    made backend and frontend  folder + install packages for backend + added post request test to folder

--- backend/baby_namer_project/settings.py
+++ backend/baby_namer_project/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'baby_namer'
+    'baby_namer',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'baby_namer_project.urls'
@@ -112,6 +114,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL=True
 
 
 try:
